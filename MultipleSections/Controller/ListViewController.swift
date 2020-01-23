6b9f6430d1d245cf9a5830507a0e75d36d06f132
Dmitrii Zavodnov <dmitrii.zavodnov@mail.ru>
2020-01-24 00:58:37 +0300

    Initial Commit

---
+++ MultipleSections/Controller/ListViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ListViewController.swift
+//  MultipleSections
+//
+//  Created by Алексей Пархоменко on 13.01.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    let sections = Bundle.main.decode([MSection].self, from: "model.json")
+    var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        view.backgroundColor = .orange
+        setupCollectionView()
+    }
+    
+    func setupCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createCompositionalLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = #colorLiteral(red: 0.968627451, green: 0.9725490196, blue: 0.9921568627, alpha: 1)
+        view.addSubview(collectionView)
+        
+        collectionView.register(ActiveChatCell.self, forCellWithReuseIdentifier: ActiveChatCell.reuseId)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    // MARK: - Setup Layout
+    
+    func createCompositionalLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            let section = self.sections[sectionIndex]
+            
+            switch section.type {
+            default:
+                return self.createActiveChatSection()
+            }
+        }
+        
+        return layout
+    }
+    
+    func createActiveChatSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(86))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets.init(top: 0, leading: 0, bottom: 8, trailing: 0)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .estimated(1))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets.init(top: 66, leading: 20, bottom: 0, trailing: 20)
+        return section
+    }
+    
+    // section -> groups -> items -> size
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+
+extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return sections[section].items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ActiveChatCell.reuseId, for: indexPath) as! ActiveChatCell
+        let section = sections[indexPath.section]
+        let item = section.items[indexPath.item]
+        cell.configure(with: item)
+        return cell
+    }
+}
+
+// MARK: - SwiftUI
+import SwiftUI
+struct ListProvider: PreviewProvider {
+    static var previews: some View {
+        ContainterView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainterView: UIViewControllerRepresentable {
+        
+        let listVC = ListViewController()
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ListProvider.ContainterView>) -> ListViewController {
+            return listVC
+        }
+        
+        func updateUIViewController(_ uiViewController: ListProvider.ContainterView.UIViewControllerType, context: UIViewControllerRepresentableContext<ListProvider.ContainterView>) {
+            
+        }
+    }
+}
+
+
+
